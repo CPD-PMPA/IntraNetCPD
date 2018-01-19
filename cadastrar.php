76c87723c8d8a005987f1bb348bdccec20604432
Vagner Kuntz <30324117+vagnerkuntz@users.noreply.github.com>
2018-01-19 15:25:56 -0200

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,15 +1,22 @@
 <?php
     require 'config.php';
+    $nome = $_POST['nome'];
+    $matricula = $_POST['matricula']
+    $telefone = $_POST['telefone'];
+    $secretaria = $_POST['secretaria'];
+    $patrimonio = $_POST['patrimonio'];
+    $descricao = $_POST['descricao']
+    $tipo_chamado = $_POST['tipo_chamado'];
 
     $sql = 'INSERT INTO cadastro (nome, matricula, telefone, secretaria, patrimonio, descricao, tipo_chamado) VALUES (:nome, :matricula, :telefone, :secretaria, :patrimonio, :descricao, :tipo_chamado)';                                      
     $e = $pdo->prepare($sql);
-    $e->bindParam(':nome', $_POST['nome']);       
-    $e->bindParam(':matricula', $_POST['matricula']); 
-    $e->bindParam(':telefone', $_POST['telefone']);
-    $e->bindParam(':secretaria', $_POST['secretaria']); 
-    $e->bindParam(':patrimonio', $_POST['patrimonio']);
-    $e->bindParam(':descricao', $_POST['descricao']);
-    $e->bindParam(':tipo_chamado', $_POST['tipo_chamado']);
+    $e->bindParam(':nome', $nome);       
+    $e->bindParam(':matricula', $matricula); 
+    $e->bindParam(':telefone', $telefone);
+    $e->bindParam(':secretaria', $secretaria); 
+    $e->bindParam(':patrimonio', $patrimonio);
+    $e->bindParam(':descricao', $descricao);
+    $e->bindParam(':tipo_chamado', $tipo_chamado);
     $e->execute();
 
     echo 'Sucesso';
