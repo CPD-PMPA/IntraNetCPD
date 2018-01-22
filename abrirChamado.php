b4eee6109987d40a446f6efaf333ebcd140d9bf7
EGGabriel <ps.eggabriel@gmail.com>
2018-01-22 15:14:56 -0200

    Add Files

--- abrirChamado.php
+++ abrirChamado.php
@@ -1,50 +1,49 @@
 <?php include 'header.inc.php'; ?>
 
-<h1>Abertura de Chamados - Departamento de TI</h1> 
+<h1>Abertura de Chamados - Departamento de TI</h1>
+<br>
+<div class = "fontup">Obs.: A abertura de falsos chamados serão tomados medidas administrativas em cima das mesmas! </div>
+<br><br>
+
 <form action="cadastrar.php" method="POST">
 Tipo De Chamado
 <select class="campo" name="tipo_chamado" required>
-<option class="campo">Selecione o Tipo de Chamado</option>
 <option value="Internet">Internet</option>
 <option value="Computador">Computador</option>
 <option value="Impressora">Impressora</option>
 <option value="Telefone">Telefone</option>
 </select>
-
-<select name="Internet" hidden>
+<select name="tipo_especifico" hidden>
 <option value="internet_um_pc">Sem Conectividade 1 Computador</option>
 <option value="internet_rede_toda">Sem Conectividade Rede Toda</option>
 </select>
-
-<select name="Computador" hidden>
+<select name="tipo_especifico" hidden>
 <option value="computador_cpu">CPU</option>
 <option value="computador_monitor">Monitor</option>
 <option value="computador_kit">Teclado/Mouse</option>
 <option value="computador_software">Software</option>
 </select>
-
-<select name="Impressora" hidden>
+<select name="tipo_especifico" hidden>
 <option value="impressora_locada">Locada</option>
 <option value="impressora_patrimonio">Patrimonio</option>
 </select>
-
-<select name="Telefone" hidden>
+<select name="tipo_especifico" hidden>
 <option value="telefone_ip">Telefone IP</option>
 <option value="telefone_comun">Telefone Comun</option>
 </select>
 <br/>
   
-<!-- Por que de todos esses autofocus ? -->
-Nome <input type="text" class="campo" name="nome" placeholder="Nome" autofocus required />
-Matricula <input type="text" class="campo" name="matricula" placeholder="Matricula" autofocus required /><br/>
+<input type="text" class="campo350" name="nome" placeholder="Nome"  required />
+<input type="text" class="campo150" name="matricula" placeholder="Matricula" maxlength="6" required /><br/>
 
-Telefone <input type="text" class="campo" name="telefone" placeholder="Telefone" autofocus required />
-Secretaria <input type="text" class="campo" name="secretaria" placeholder="Secretaria" autofocus required /><br/>
+<input type="text" class="campo150 cel" name="telefone" placeholder="Tel p/ Contato" maxlength="16" required />
+<input type="text" class="campo350" name="secretaria" placeholder="Secretaria"  required /><br/>
 
-Patrimônio <input type="text" class="campo" name="patrimonio" placeholder="Patrimonio" autofocus required /><br/>
-Observação <textarea class="campo" data-ls-module="charCounter" maxlength="200" placeholder="Observação"></textarea><br/>
-
+<input type="text" class="campo500" name="descricao" placeholder="Descrição do Problema" maxlength="200" required /><br/>
 <input type="submit" value="Abrir Chamado" /> 
 
 </form>
+
+
+<script type="text/javascript" src="script/abrirChamado.js"></script>   
 <?php include 'footer.inc.php'; ?>
--- cadastrar.php
+++ cadastrar.php
@@ -1,25 +1,25 @@
 <?php
     require 'config.php';
     $nome = $_POST['nome'];
-    $matricula = $_POST['matricula']
+    $matricula = $_POST['matricula'];
     $telefone = $_POST['telefone'];
     $secretaria = $_POST['secretaria'];
-    $patrimonio = $_POST['patrimonio'];
-    $descricao = $_POST['descricao']
+    // $patrimonio = $_POST['patrimonio'];
+    $descricao = $_POST['descricao'];
     $tipo_chamado = $_POST['tipo_chamado'];
-    //$tipo_especifico = $_POST['tipo_especifico'];
+    // $tipo_especifico = $_POST['tipo_especifico'];
 
     // Inserir caso nescessario tipo_especifico na SQL.
-    $sql = 'INSERT INTO cadastro (nome, matricula, telefone, secretaria, patrimonio, descricao, tipo_chamado) VALUES (:nome, :matricula, :telefone, :secretaria, :patrimonio, :descricao, :tipo_chamado)';                                      
+    $sql = 'INSERT INTO `abertura_chamado` (nome, matricula, telefone, secretaria, descricao, tipo_chamado) VALUES (:nome, :matricula, :telefone, :secretaria, :descricao, :tipo_chamado)';                                      
     $e = $pdo->prepare($sql);
     $e->bindParam(':nome', $nome);       
     $e->bindParam(':matricula', $matricula); 
     $e->bindParam(':telefone', $telefone);
     $e->bindParam(':secretaria', $secretaria); 
-    $e->bindParam(':patrimonio', $patrimonio);
+    // $e->bindParam(':patrimonio', $patrimonio);
     $e->bindParam(':descricao', $descricao);
     $e->bindParam(':tipo_chamado', $tipo_chamado);
-    //$e->bindParam(':tipo_especifico', $tipo_especifico);
+    // $e->bindParam(':tipo_especifico', $tipo_especifico);
     $e->execute();
 
-    echo 'Sucesso';
+    echo("Seu chamado foi aberto com sucesso! Aguarde nossa equipe verificar e tomar providências!");
